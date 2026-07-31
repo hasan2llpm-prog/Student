dacff2f8ce19f8ad2898d2375000555a62d17bf4
hasan2llpm-prog <hasan.2.llpm@gmail.com>
2026-07-31 11:33:18 +0300

    reels-safety.js

---
+++ reels-safety.js
@@ -0,0 +1,15 @@
+/* =========================================================
+   Student - Reels Safety
+   🚩 الإبلاغ
+   🚫 إخفاء Reel
+   🔗 نسخ الرابط
+========================================================= */
+
+(function () {
+
+    "use strict";
+
+    window.StudentReelsSafety =
+        window.StudentReelsSafety || {};
+
+})();
